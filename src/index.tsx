e12a1165682c8b75077c3ea1c6cc81bfde95e3b4
Mrigankashekhar Shandilya <mrigankashekhar2107@gmail.com>
2025-09-25 03:03:37 +0530

    first web commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+    return (
+        <div>
+            {/* Your content goes here */}
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
